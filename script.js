eb9663ba73b3df0bd72002b95facde974de7ac61
y2k <Iman@Macintosh.local>
2022-06-09 12:29:36 -0400

    add js function for color coded time blocks

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 
 var timeBlockARR=[9,10,11,12,13,14,15,16,17]
-var cuurentHour = moment().hour()//get current hour
+var currentHour = moment().hour()//get current hour
 var currentDay= $("#currentDay")
 var systemTime = moment().format("dddd, MMMM Do")
 
 currentDay.text(systemTime)//text content in js
+function displayTimeColor(){
+    for(var i=0;i<timeBlockARR.length;i++){
+        var currentTextEL = $("#"+timeBlockARR[i])
+        if(currentHour>timeBlockARR[i]){
+           
+           currentTextEL.addClass("past")
+        }
+        else if(currentHour == timeBlockARR[i]){
+            currentTextEL.addClass("present")
+
+        }else{currentTextEL.addClass("future")}
+    }
+}
+displayTimeColor()
 
